c8bf9fc542ab12f29ca8db443c64c4fd0dee9c32
CB-Shantikumar <tshantikumarsingh@mban-tshantikumar.local>
2016-04-13 19:35:50 +0530

    Post resume page
    Post resume page

--- js/uresume.js
+++ js/uresume.js
@@ -3,8 +3,20 @@ $(document).ready(function() {
         var fileName = e.target.files[0].name;
         $('#uploadedFile').html(fileName);
         $('#showFileName').removeClass('hide').addClass('show-name');
-        $('#fileUpload').hide();
-        $('.existing-resume').hide();
+        $('#fileUpload').addClass('hide').removeClass('show');
+        $('.existing-resume').addClass('hide');
     });
+
+    
+
+    $('.tgl input').change(function(){
+        if($(this).is(':checked')){
+            $('#progress1').addClass('step4');
+            $('#pb-4').show();
+        }else{
+            $('#progress1').removeClass('step4');
+            $('#pb-4').hide();
+        }
+    })
 
 });
